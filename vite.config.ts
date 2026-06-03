20c470d28de01fb7c3ee4b3f465a6de65e2d8572
naimaktas <naimaktasx@gmail.com>
2026-06-03 11:53:40 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -206,6 +206,7 @@ function vitePluginStorageProxy(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector(), vitePluginStorageProxy()];
 
 export default defineConfig({
+  base: '/naim-aktas-official/',
   plugins,
   resolve: {
     alias: {
